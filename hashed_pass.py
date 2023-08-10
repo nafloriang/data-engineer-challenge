e827f0d733e9fc59940cc69977f114b184c88abf
Alejandro Florián <nafloriang@gmail.com>
2023-08-09 21:39:31 -0500

    icambios añadidos# Please enter the commit message for your changes. Lines starting

--- hashed_pass.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-
-password = "nueva_contraseña"
-hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-hashed_password = hashed.decode('utf-8')  # Convertir a string para guardar en SQLite.
